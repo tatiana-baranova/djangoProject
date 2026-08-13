1d3cc4c0c69d61f232fc93e651c3962453fec99d
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-14 01:26:15 +0300

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+class News(models.Model):
+    title = models.CharField('Назва статті', max_length=100)
+    text = models.TextField('Основний текст статті')
+    date = models.DateTimeField('Дата опублікування', default=timezone.now)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.title}'
+    class Meta:
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
