57cae60d68e03b509461ccd7336b65f16d0a598f
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-15 22:26:06 +0300

    add registration page

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reg/', userViews.register, name='reg'),
     path('', include('core.urls'))
+    
 ]
