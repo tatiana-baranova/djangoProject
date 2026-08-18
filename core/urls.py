6d3b4b86e3905580986ad3d4bb391fa574a804a7
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-18 17:56:08 +0300

    add articles page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('uslugi', views.services, name='services'),
+    path('articles', views.ShowNewsView.as_view(), name='articles'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import News
+from django.views.generic import ListView
+
 def home(request):
     data = {
         'title': 'Головна сторінка'
@@ -15,3 +17,15 @@ def services(request):
     }
 
     return render(request, 'core/services.html', data)
+
+class ShowNewsView(ListView):
+    model = News
+    template_name = 'core/articles.html'
+    context_object_name = 'news'
+    ordering = ['-date']
+
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = 'Сторінка з статтями'
+        return ctx
